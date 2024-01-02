0aa92f22a6f33442d11d8431e2a79e3bbc327925
stech” <smitsavaliya321@gmail.com>
2024-01-02 20:35:34 +0530

    expressjs

--- controller/order.controller.js
+++ controller/order.controller.js
@@ -6,14 +6,14 @@ exports.addNewOrder = async (req, res) => {
     try {
         // find cartitems and joint with populate
         const cartItems = await Cart.find({user:req.user._id,isDelete:false}).populate('cartItem');
-        // console.log(cartItems);
+        console.log(cartItems);
         // caritmes make array of product items and after use map and acess from array
         let orderItems = cartItems.map((ele)=>({
             cartItem: ele.cartItem,
             quantity: ele.quantity,
             price: ele.cartItem.price
         }));
-        // console.log(orderItems);
+        console.log(orderItems);
         // use reduce to price Quntity * price 
         let totalPrice = orderItems.reduce((total, item)=> total + (item.quantity * item.price),0);
         // console.log(totalPrice);
@@ -34,3 +34,98 @@ exports.addNewOrder = async (req, res) => {
         res.json({message: 'Server Error'});
     }
 }
+
+exports.cancelOrder = async (req,res)=>{
+    try {
+        let orderId = new mongoose.Types.ObjectId(req.query.orderId);
+        let order = await Order.findOne({user:req.user._id,isDelete:false})
+        if(!order){
+            return res.json({message:"order not found"});
+        }
+        let deleteOrder = await Order.updateOne({_id:orderId},{isDelete:true})
+        res.json({message:"cancel order sucessfully...",order:deleteOrder})
+        
+    } catch (error) {
+        console.log(error);
+        res.json({message: 'Server Error'});
+    }
+}
+
+exports.getAllOrder = async (req,res)=>{
+try {
+    let order = await Order.findOne({user:req.user._id,isDelete:false})
+        if(!order){
+            return res.json({message:"order not found"});
+        }
+        res.json({massage:"all orders found",order})
+} catch (error) {
+    console.log(error);
+    res.json({message: 'Server Error'});
+}
+}
+
+exports.getOrder = async (req,res)=>{
+   try {
+     let orderId = new mongoose.Types.ObjectId(req.query.id)
+     console.log(orderId);
+     let order = await Order.findOne({_id:orderId,isDelete:false})
+     if (!order) {
+        return res.json({massage:"order not found"})
+     } 
+     res.json({massage:"spacific order found ",order})
+   } catch (error) {
+    console.log(error);
+    res.json({message: 'Server Error'});
+   }
+}
+
+// exports.updateOrder = async (req,res)=>{
+//     try {
+//         const { id, quantity, cartItem } = req.body;
+//     let order = await Order.findOne({ user: req.user._id, _id: id });
+//     if (!order) {
+//           return res.json("You have not any order");
+//         }
+
+//         order = await Cart.findOne({
+//           cartItem: cartItem,
+//           isDelete: true,
+//         });
+//         if (!order) {
+//           return res.json("Item is not ordered You");
+//         }
+
+//         order = await Cart.updateOne(
+//           { cartItem: cartItem },
+//           {
+//             $set: { quantity: quantity },
+//           },
+//           { new: true }
+//         );
+    
+//         order = await Cart.find({ user: req.user._id, isDelete: true }).populate("cartItem");
+    
+//         let orderItem = order.map((ele) => ({
+//           cartItem: ele.cartItem._id,
+//           quantity: ele.quantity,
+//           price: ele.cartItem.price,
+//         }));
+    
+//         let totalPrice = orderItem.reduce((total, item) => (total += item.quantity * item.price), 0);
+        
+//         let updateOrder = await Order.findOneAndUpdate(
+//           { user: req.user._id },
+//           {
+//             $set: { items: orderItem, totalAmount: totalPrice },
+//           },
+//           { new: true }
+//         );
+    
+//         updateOrder.save();
+//         res.json({ message: "update order success", updateOrder });
+//       } catch (err) {
+//         console.log(err);
+//         res.json("Internal server error");
+//       }
+//     };
+    
--- routes/order.routes.js
+++ routes/order.routes.js
@@ -1,7 +1,12 @@
 const orderRoutes = require("express").Router();
 const { verifyToken } = require("../helpers/verifyToken");
-const { addNewOrder } = require("../controller/order.controller");
+const { addNewOrder ,cancelOrder ,getAllOrder, getOrder ,updateOrder} = require("../controller/order.controller");
 
 orderRoutes.post('/add-order', verifyToken, addNewOrder);
+orderRoutes.delete('/cancel-order',verifyToken,cancelOrder)
+orderRoutes.get('/orders',verifyToken,getAllOrder)
+orderRoutes.get('/order',verifyToken,getOrder)
+// orderRoutes.patch('/update-order',verifyToken,updateOrder)
+
 
 module.exports = orderRoutes;
